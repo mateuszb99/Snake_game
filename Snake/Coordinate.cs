ec792a4e2a6694a1b7d7db9d093fb15f9e14bd78
mateuszb99 <mateusz.baran03@gmail.com>
2021-06-24 17:38:45 +0200

    Initial commit

---
+++ Snake/Coordinate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Coordinate
+    {
+        public Coordinate()
+        {
+            X = 0;
+            Y = 0;
+        }
+        public Coordinate(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
---
+++ Snake/Meal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Meal
+    {
+        public Meal()
+        {
+            Random rand = new Random();
+            var x = rand.Next(1, 20);
+            var y = rand.Next(1, 20);
+            CurrentTarget = new Coordinate(x, y);
+            Draw();
+
+
+        }
+
+        public Coordinate CurrentTarget { get; set; }
+
+
+        void Draw()
+        {
+            Console.SetCursorPosition(CurrentTarget.X, CurrentTarget.Y);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("$");
+
+        }
+    }
+}
